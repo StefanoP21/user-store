3aded418ccfb35f0be8767e1251ebd3bb8c401d3
Stefano <stefanop21@outlook.es>
2024-06-28 21:53:50 -0500

    update: create user - handle errors

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from 'express';
-import { RegisterUserDto } from '../../domain';
+import { CustomError, RegisterUserDto } from '../../domain';
 import { AuthService } from '../services/auth.service';
 
 export class AuthController {
   //* DI
   constructor(public readonly authService: AuthService) {}
+
+  private handleError = (res: Response, error: unknown) => {
+    if (error instanceof CustomError) {
+      return res.status(error.statusCode).json({ error: error.message });
+    }
+
+    return res.status(500).json({ error: 'Internal server error' });
+  };
 
   registerUser = (req: Request, res: Response) => {
     const [error, registerUserDto] = RegisterUserDto.register(req.body);
@@ -13,8 +21,8 @@ export class AuthController {
 
     this.authService
       .registerUser(registerUserDto!)
-      .then((user) => res.status(200).json({ user }))
-      .catch((error) => res.status(400).json({ error }));
+      .then((user) => res.status(200).json(user))
+      .catch((error) => this.handleError(res, error));
   };
 
   loginUser = (req: Request, res: Response) => {
--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -1,15 +1,30 @@
 import { UserModel } from '../../data';
-import { CustomError, RegisterUserDto } from '../../domain';
+import { CustomError, RegisterUserDto, UserEntity } from '../../domain';
 
 export class AuthService {
   //* DI
   constructor() {}
 
-  public async registerUser(dto: RegisterUserDto) {
-    const existUser = await UserModel.findOne({ email: dto.email });
+  public async registerUser(registerUserDto: RegisterUserDto) {
+    const existUser = await UserModel.findOne({ email: registerUserDto.email });
 
     if (existUser) throw CustomError.badRequest('Email already exist');
 
-    return 'ok';
+    try {
+      const user = new UserModel(registerUserDto);
+      await user.save();
+
+      //todo: encrypt password
+
+      //todo: jwt
+
+      //todo: send email
+
+      const { password, ...userEntity } = UserEntity.fromObject(user);
+
+      return { user: userEntity, token: ' abc' };
+    } catch (error) {
+      throw CustomError.internalServerError(`${error}`);
+    }
   }
 }
