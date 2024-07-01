473a260bdbd4992c61159d8bea3141dc6dbd8f61
Stefano <stefanop21@outlook.es>
2024-07-01 18:57:57 -0500

    new: email - service

---
+++ src/presentation/services/email.service.ts
@@ -0,0 +1,44 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import { Attachment } from 'nodemailer/lib/mailer';
+
+export interface SendEmailOptions {
+  to: string | string[];
+  subject: string;
+  html: string;
+  attachments?: Attachment[];
+}
+
+export class EmailService {
+  private transporter: Transporter;
+
+  constructor(
+    public readonly mailerService: string,
+    public readonly mailerEmail: string,
+    public readonly senderEmailPassword: string
+  ) {
+    this.transporter = nodemailer.createTransport({
+      service: mailerService,
+      auth: {
+        user: mailerEmail,
+        pass: senderEmailPassword,
+      },
+    });
+  }
+
+  async sendEmail(options: SendEmailOptions): Promise<boolean> {
+    const { to, subject, html, attachments = [] } = options;
+
+    try {
+      await this.transporter.sendMail({
+        to,
+        subject,
+        html,
+        attachments,
+      });
+
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+}
