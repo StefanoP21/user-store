2ba6251c63ecb9ab7d2deed26fae333196c5334c
Stefano <stefanop21@outlook.es>
2024-07-04 15:36:34 -0500

    new: create product - dto

---
+++ src/domain/dtos/product/create-product.dto.ts
@@ -0,0 +1,30 @@
+export class CreateProductDto {
+  private constructor(
+    public readonly name: string,
+    public readonly available: boolean,
+    public readonly price: number,
+    public readonly description: string,
+    public readonly user: string,
+    public readonly category: string
+  ) {}
+
+  static create(object: { [key: string]: any }): [string?, CreateProductDto?] {
+    const { name, available, price, description, user, category } = object;
+
+    if (!name) return ['Missing name', undefined];
+    if (!user) return ['Missing user', undefined];
+    if (!category) return ['Missing category', undefined];
+
+    return [
+      undefined,
+      new CreateProductDto(
+        name,
+        !!available,
+        price,
+        description,
+        user,
+        category
+      ),
+    ];
+  }
+}
