a7f0ae430de988ba5d0ced12736a00b9d49597cb
Stefano <stefanop21@outlook.es>
2024-07-13 13:28:00 -0500

    feat: file upload controller - route

---
+++ src/presentation/file-upload/controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { CustomError } from '../../domain';
+
+export class FileUploadController {
+    // constructor(private readonly categoryService: CategoryService) {}
+
+    private handleError = (res: Response, error: unknown) => {
+        if (error instanceof CustomError) {
+            return res.status(error.statusCode).json({ error: error.message });
+        }
+
+        return res.status(500).json({ error: 'Internal server error' });
+    };
+
+    uploadFile = async (req: Request, res: Response) => {
+        res.json('upload file');
+    };
+
+    uploadMultipleFile = async (req: Request, res: Response) => {
+        res.json('upload multiple files');
+    };
+}
